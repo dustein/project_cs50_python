64b078a0602066259b13f21376e26d8f37bd1132
dustein <steindu@gmail.com>
2026-01-15 17:03:46 -0300

    project MET

--- my-thinking.md
+++ my-thinking.md
@@ -1,5 +1,6 @@
 # Planejamento Software Final Project Harvard's CS50x Python
 
-- Web Application rodando em Flask com Documento padrão para preenchimento do BDT dos blindados com as observações, que ao final gera um PDF com comando de saída para impressora.
+- Cálculo de calorias conforme peso x MET
+
 
 
--- project.py
+++ project.py
@@ -1,9 +1,35 @@
+EXERCISES = {
+    "caminhada": 2.0,
+    "caminhada rapida" : 5.0,
+    "caminhada em trilha": 6.0,
+    "corrida": 8.3,
+    "corrida rapida": 11.5,
+    "ciclismo": 4.0,
+    "ciclismo moderado": 6.8,
+    "ginastica": 2.5,
+    "calistenia": 3.5,
+    "musculacao": 6.0,
+    "circuito": 8.0,
+    "natacao": 5.8,
+    "corda": 8.0,
+    "escadas": 4.0,
+}
+
 def main():
-    ...
+    print("Lista de Exercícios:")
+    for i, exercise in enumerate(EXERCISES.keys(), 1):        
+        print(f'{i} - {exercise}', end="\n")
+
+    user_exercise = input("Digite o nome do exercício: ")
+    met_value = verify_met(user_exercise)
+
+    print(met_value)
 
 
-def function_1():
-    ...
+def verify_met(exercise):
+    exercise_name = exercise.lower().strip()
+    print(exercise)
+    return exercise_name
 
 
 def function_2():
